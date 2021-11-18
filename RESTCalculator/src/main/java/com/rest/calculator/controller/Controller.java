2c608806fcb277c58c2b81bc9614fce790a79907
nguyenductien1 <ductien692@gmail.com>
2021-11-18 15:00:06 +0100

    add controller

--- RESTCalculator/src/main/java/com/rest/calculator/controller/Controller.java
+++ RESTCalculator/src/main/java/com/rest/calculator/controller/Controller.java
@@ -1,13 +1,79 @@
 package com.rest.calculator.controller;
 
+
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+
+import java.io.IOException;
+
+import com.rest.calculator.services.MultiplyService;
+
+
 
 @RestController
 public class Controller {
-	//Ici on crée les URL d'écoute
-	//Exemple
+	
+	private MultiplyService multiply;
 	
 	
+	@PostMapping("/plus")
+	public ResponseEntity<String> plus( @RequestBody String entree) {
+		
+		int result;
+		JSONObject entreeJSON = new JSONObject(entree);
+		int nba = entreeJSON.getInt('nba');
+		result = multiply.plus(nba, nbb);
+		String response = Integer.toString(result);
+		
+		
+		return new ResponseEntity<String>(response, HttpStatus.OK);
+		
+	}
+	
+	@PostMapping("/multiply")
+	public ResponseEntity<String> multiply( @RequestBody int nba, @RequestBody int nbb) throws IOException{
+		
+		int result;
+		
+		result = multiply.multiply(nba, nbb);
+		String response = Integer.toString(result);
+		
+		return new ResponseEntity<String>(response, HttpStatus.OK);
+		
+	}
+
+	
+	@PostMapping("/devide")
+	public ResponseEntity<String> divide( @RequestBody int nba, @RequestBody int nbb) throws IOException{
+		
+		int result;
+		
+		result = multiply.divide(nba, nbb);
+		String response = Integer.toString(result);
+		
+		return new ResponseEntity<String>(response, HttpStatus.OK);
+		
+	}
+	
+	@PostMapping("/moin")
+	public ResponseEntity<String> moin( @RequestBody int nba, @RequestBody int nbb) throws IOException{
+		
+		int result;
+		
+		result = multiply.moins(nba, nbb);
+		String response = Integer.toString(result);
+		
+		return new ResponseEntity<String>(response, HttpStatus.OK);
+		
+	}
+	
+	
+
 	
 	
 	
