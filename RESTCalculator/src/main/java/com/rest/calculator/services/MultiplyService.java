596b7e5a5fcccce2c6515a2017637e71b0b03937
nguyenductien1 <ductien692@gmail.com>
2021-11-18 15:37:22 +0100

    multiply controller final

--- RESTCalculator/src/main/java/com/rest/calculator/services/MultiplyService.java
+++ RESTCalculator/src/main/java/com/rest/calculator/services/MultiplyService.java
@@ -5,18 +5,8 @@ public class MultiplyService {
 	public double multiply(double a, double b) {
         return a * b;
     }
-<<<<<<< HEAD
-	public int plus(int a, int b) {
-        return a + b;
-    }
-	public int moins(int a, int b) {
-        return a - b;
-    }
-	public int divide(int a, int b) {
-        return a / b;
-    }
-=======
-	
+
+		
 	public double multiply(double a) {
 		return a;
 	}
@@ -24,5 +14,5 @@ public class MultiplyService {
 	public double multiply() {
 		return 0;
 	}
->>>>>>> 5dd0513ea2fd7553cee3ccdd168ab5344f65c3ea
+
 }
