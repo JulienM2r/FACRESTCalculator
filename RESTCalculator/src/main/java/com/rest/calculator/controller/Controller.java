e506eae6e1a94c87f8f59fad085864709f01a37b
JulienM2r <jum2r@hotmail.com>
2021-11-18 12:33:26 +0100

    push some file

---
+++ RESTCalculator/src/main/java/com/rest/calculator/controller/Controller.java
@@ -0,0 +1,18 @@
+package com.rest.calculator.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	//Ici on crée les URL d'écoute
+	//Exemple
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
