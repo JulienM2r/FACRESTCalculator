466b6e0e4b9df55c9b7733734303e55774cc34a8
nguyenductien1 <ductien692@gmail.com>
2021-11-18 15:07:29 +0100

    add

--- RESTCalculator/src/main/java/com/rest/calculator/services/MultiplyService.java
+++ RESTCalculator/src/main/java/com/rest/calculator/services/MultiplyService.java
@@ -5,4 +5,13 @@ public class MultiplyService {
 	public int multiply(int a, int b) {
         return a * b;
     }
+	public int plus(int a, int b) {
+        return a + b;
+    }
+	public int moins(int a, int b) {
+        return a - b;
+    }
+	public int divide(int a, int b) {
+        return a / b;
+    }
 }
